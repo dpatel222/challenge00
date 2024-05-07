9fceffa465e03a49aaeb049d67d024d9a1353b22
dpatel222 <deeppatel2@rrc.ca>
2024-05-07 11:31:09 -0500

    first commit

---
+++ assignment0.rb
@@ -0,0 +1,17 @@
+# Deep Patel
+# Challenge Zero
+
+puts "enter a number"
+sub_total = gets.chomp.to_i
+PST = 0.07 * sub_total
+GST = 0.05 * sub_total
+grand_total = sub_total + PST + GST
+puts(grand_total)
+
+if(grand_total <= 5)
+    puts("Pocket Change")
+elsif(grand_total > 5 && grand_total < 20 )
+    puts("Wallet Time")
+else (grand_total >= 20)
+    puts("Change It") 
+end
